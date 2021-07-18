1e1725240d1ea930369a8242d4263535fda03615
Vijayalakshmi Sampath <61058491+VijayalakshmiSam@users.noreply.github.com>
2021-07-18 22:17:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='Vijisam@12345'
+app.config['MYSQL_PASSWORD']='your_password'
 app.config['MYSQL_DB']='fooddelivery'
 
 mysql=MySQL(app)
